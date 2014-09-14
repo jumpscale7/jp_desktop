aac67b2c723978051ae62ad14a06ae3f125f9f96
despiegk <kristof@incubaid.com>
2014-09-14 16:39:11 +0200

    desktop apps

--- jpackages/kingsoftoffice/1.0/actions/install.prepare.py
+++ jpackages/kingsoftoffice/1.0/actions/install.prepare.py
@@ -9,17 +9,27 @@ def main(j,jp):
     #args.qp.installUbuntuDebs()
     
     #shortcut to some usefull install tools
-    do=j.system.installtools
 
+    
+    execute=j.system.process.executeWithoutPipe
     #configuration is not done in this step !!!!!
     #copying files from files section of jpackages is not done in this step
 
+    print "enable system for 32 bit"
     cmd1="dpkg --add-architecture i386"
-    do.execute(cmd1)
+    execute(cmd1)
 
-    # cmd2="apt-get update"
-    # do.execute(cmd2)
+    print "update apt"
+    cmd2="apt-get update"
+    execute(cmd2)    
 
-    cmd3="apt-get install libx11-6:i386 libxmu6:i386 libxext6:i386 libxrender1:i386 libstdc++6:i386 libc6:i386 -y"
-    do.execute(cmd3)
+    print "install ia32 libs"
+    cmd3="apt-get install libglib2.0-0:i386 libpng12-0:i386 libfontconfig1:i386 libfreetype6:i386 libglu1-mesa:i386 libcups2:i386 libx11-6:i386 libxmu6:i386 libxext6:i386 libxrender1:i386 libstdc++6:i386 libc6:i386 -y"
+    execute(cmd3)
+
+    print "install truetype fonts"
+    cmd3="apt-get install ttf-mscorefonts-installer:i386 -y"
+    execute(cmd3)
+
+    print "should now be 32 bit"
     
